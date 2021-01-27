f1895e283bd4976147813cc65b623a45b225e8f1
bondesvick <bondesvick@gmail.com>
2021-01-27 18:47:24 +0100

    completed summary area

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -9,9 +9,8 @@ const Main = styled.div`
     border-radius: 6px;
     display: flex;
     flex-direction: column;
-    position: relative;
-    justify-content: space-around;
-    padding-left: 16px;
+    justify-content: flex-start;
+    padding: 9px;
 
     grid-column-start: 1;
     grid-column-end: 2;
@@ -19,9 +18,60 @@ const Main = styled.div`
     grid-row-end: 4;
 `
 
+const Addresses = styled.div`
+     display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    .anAddress{
+        display: flex;
+         width: 100%;
+         align-items: center;
+         margin-bottom: -14px;
+
+         p{
+             font-size: 14px;
+             line-height: 140%;
+         }
+    }
+
+`
+
 export default function Summary() {
     return (
         <Main>
+            <div style={{display: "flex", width: "100%", alignItems: "center", marginBottom: "-10px"}}>
+                <img src="./Images/Castle.png" alt="castle" style={{marginRight: "12px"}}/>
+                <p style={{fontWeight: "bold"}}>Division Summary</p>
+            </div>
+
+            <Addresses>
+                <div className="anAddress">
+                    <img src="./Images/phone.png" alt="castle" style={{height: "16px", width: "16px", marginRight: "12px"}}/>
+                    <p style={{}}>08012345678</p>
+                </div>
+
+                <div className="anAddress">
+                    <img src="./Images/envelope.png" alt="castle" style={{height: "16px", width: "16px", marginRight: "12px"}}/>
+                    <p style={{}}>asbfefr@gmail.com</p>
+                </div>
+
+                <div className="anAddress">
+                    <img src="./Images/location.png" alt="castle" style={{height: "16px", width: "16px", marginRight: "12px"}}/>
+                    <p style={{}}>Mojidi, Lagos</p>
+                </div>
+
+                <div className="anAddress">
+                    <img src="./Images/notes-book.png" alt="castle" style={{height: "16px", width: "16px", marginRight: "12px"}}/>
+                    <p style={{color: "#5771EE", textDecoration: "underline"}}>2 Journal entries</p>
+                </div>
+
+                <div className="anAddress">
+                    <img src="./Images/Group.png" alt="castle" style={{height: "16px", width: "16px", marginRight: "12px"}}/>
+                    <p style={{}}>24 fingerprints enrolled</p>
+                </div>
+            </Addresses>
+
             
         </Main>
     )
