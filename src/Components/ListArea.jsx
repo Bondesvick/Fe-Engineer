6008397596974aeaf2bb57e9e897d82f2976c380
bondesvick <bondesvick@gmail.com>
2021-01-27 12:02:06 +0100

    added list ite component items

--- src/Components/ListArea.jsx
+++ src/Components/ListArea.jsx
@@ -7,20 +7,100 @@ const Main = styled.div`
     height: 100%;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
     border-radius: 0 0 6px 6px;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    justify-content: space-around;
+    overflow-y: hidden;
 
     grid-column-start: 2;
     grid-column-end: 6;
     grid-row-start: 2;
     grid-row-end: 6;
+
+
 `
+
+const Top = styled.div`
+    width: 100%;
+    height: 45px;
+    background-color: #F3F6F9;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    .chkbx{
+        height: 15px;
+        width: 15px;
+        border: 5px solid #9F9F9F;
+        margin-left: 20.5px;
+    }
+
+`
+
+const Content = styled.div`
+    overflow-y: auto;
+    width: 100%;
+    height: 100%;
+
+    /* scrollbar,
+    scrollbar-thumb {
+        width: 26px;
+        border-radius: 13px;
+        background-clip: padding-box;
+        border: 10px solid transparent;
+    }  */
+`
+
+const ListItem = styled.div`
+    height: 60px;
+    width: 100%;
+    border-bottom: 1px solid #DCE1E7;
+    display: flex;
+    align-items: center;
+
+    .chkbx{
+        height: 15px;
+        width: 15px;
+        border: 5px solid #9F9F9F;
+        margin-left: 20.5px;
+    }
+
+`
+
+function HeadItem({val}){
+    return(<div>
+        <p>{val}</p>
+    </div>)
+}
 
 export default function ListArea() {
     return (
         <Main>
+            <Top>
+                <input className="chkbx" type="checkbox"/>
+                <HeadItem val="NAME"/>
+                <HeadItem val="LOCATION"/>
+                <HeadItem val="STATUS"/>
+                <HeadItem val="ENTERIES"/>
+                <HeadItem val="RISK PROFILE"/>
+            </Top>
+            <Content>
+                <ListItem>
+                    <input className="chkbx" type="checkbox"/>
+                </ListItem>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+            </Content>
+            
             
         </Main>
     )
